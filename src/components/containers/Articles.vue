416de84da3631b48796e1b3bac4e70d53a371a95
Gary Fagan <garyfagan1987@googlemail.com>
2020-03-14 15:19:20 +0000

    creating articles component

---
+++ src/components/containers/Articles.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="home">
+    <alert v-if="error" />
+    <div v-if="loaded">
+        <Card :article="article" v-for="article in articles" :key="article.name" />
+        <div v-if="articles.length > 0">
+            <Button
+                :click="getMoreArticles"
+                :disabled="articles.length === total"
+                label="Load more"
+            />
+        </div>
+    </div>
+    <div v-else-if="!loaded && !error">
+        <spinner />
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+import Alert from '@/components/Alert.vue';
+import Button from '@/components/Button.vue';
+import Card from '@/components/Card.vue';
+import Spinner from '@/components/Spinner.vue';
+
+Vue.use(VueAxios, axios);
+
+const START = 0;
+const PER_PAGE = 2;
+
+export default {
+  name: 'Articles',
+  components: {
+    Alert,
+    Button,
+    Card,
+    Spinner,
+  },
+  data() {
+    return {
+      articles: [],
+      end: PER_PAGE,
+      error: false,
+      loaded: false,
+      start: START,
+      total: null,
+    };
+  },
+  methods: {
+    getArticles() {
+      this.axios
+        .get(
+          `${process.env.VUE_APP_API_ARTICLES_PATH}?_start=${this.start}&_limit=${this.end}`,
+        )
+        .then((response) => {
+          this.loaded = true;
+          this.articles = this.articles.concat(response.data);
+        })
+        .catch(() => {
+          this.error = true;
+        });
+    },
+    getMoreArticles() {
+      this.start += PER_PAGE;
+      this.end = this.start + PER_PAGE;
+      this.getArticles();
+    },
+  },
+  mounted() {
+    this.getArticles();
+    this.axios
+      .get(`${process.env.VUE_APP_API_ARTICLES_PATH}/count`)
+      .then((response) => {
+        this.total = response.data;
+      })
+      .catch(() => {
+        this.error = true;
+      });
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,8 @@
 <template>
   <div class="home">
-    <alert :error="error" />
     <about />
     <div class="container">
-      <Card
-        :article="article"
-        v-for="article in articles"
-        :key="article.name"
-      />
-      <div v-if="articles.length > 0">
-        <Button :click="getMoreArticles" :disabled="articles.length === total" label="Load more" />
-      </div>
+      <articles />
     </div>
   </div>
 </template>
@@ -21,54 +13,15 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 
 import About from '@/components/containers/About.vue';
-import Alert from '@/components/Alert.vue';
-import Button from '@/components/Button.vue';
-import Card from '@/components/Card.vue';
+import Articles from '@/components/containers/Articles.vue';
 
 Vue.use(VueAxios, axios);
-
-const START = 0;
-const PER_PAGE = 2;
 
 export default {
   name: 'Home',
   components: {
     About,
-    Alert,
-    Button,
-    Card,
-  },
-  data() {
-    return {
-      articles: [],
-      end: PER_PAGE,
-      error: false,
-      start: START,
-      total: null,
-    };
-  },
-  methods: {
-    // todo, this should be the article list components job
-    getArticles() {
-      this.axios.get(`${process.env.VUE_APP_API_ARTICLES_PATH}?_start=${this.start}&_limit=${this.end}`).then((response) => {
-        this.articles = this.articles.concat(response.data);
-      }).catch(() => {
-        this.error = true;
-      });
-    },
-    getMoreArticles() {
-      this.start += PER_PAGE;
-      this.end = this.start + PER_PAGE;
-      this.getArticles();
-    },
-  },
-  mounted() {
-    this.getArticles();
-    this.axios.get(`${process.env.VUE_APP_API_ARTICLES_PATH}/count`).then((response) => {
-      this.total = response.data;
-    }).catch(() => {
-      this.error = true;
-    });
+    Articles,
   },
 };
 </script>
