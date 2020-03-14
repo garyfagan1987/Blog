f84caad3e81499e8146e9220812dcc7131388a5c
Gary Fagan <garyfagan1987@googlemail.com>
2020-03-14 14:54:30 +0000

    add spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="spinner" />
+</template>
+
+<script>
+export default {
+  name: 'Spinner',
+};
+</script>
+
+<style scoped>
+.spinner {
+  animation: spin 1s linear infinite;
+  border: 5px solid var(--color-white);
+  border-radius: 50%;
+  border-top: 5px solid var(--color-primary-darker);
+  height: 30px;
+  margin: 0 auto;
+  width: 30px;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
