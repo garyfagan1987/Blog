8e863264860566055a95b1638dd71e103e1bb63e
Gary Fagan <garyfagan1987@googlemail.com>
2020-03-15 07:13:24 +0000

    adding footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
     <router-view/>
+    <Footer />
   </div>
 </template>
 
 <script>
+import Footer from '@/components/Footer.vue';
+
 export default {
   name: 'App',
+  components: {
+    Footer,
+  },
   watch: {
     $route: function route() {
       if (this.$route.name.toLowerCase() === 'home') {
@@ -35,16 +41,24 @@ export default {
   font-size: 62.5%;
 }
 
+html, body {
+  min-height: 100%;
+}
+
 body {
   margin: 0;
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   font-size: 1.6rem;
+  height: -webkit-fill-available;
+  justify-content: space-between;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
 }
 
 .container {
@@ -67,9 +81,5 @@ a {
 
 h1, h2, h3, h4, h5, h6 {
   margin: 0;
-}
-
-#nav {
-  margin-bottom: 30px;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="footer">
+    <div class="container">
+      <p>Made with Strapi + VueJs</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style scoped>
+.footer {
+  background-color: var(--color-grey-dark);
+  color: var(--color-white);
+  margin-top: 20px;
+  padding: 10px 0;
+  text-align: center;
+}
+</style>
