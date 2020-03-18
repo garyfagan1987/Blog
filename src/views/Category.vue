ad744882460c8ac019440e26205cb1c59a9f043d
Gary Fagan <garyfagan1987@googlemail.com>
2020-03-18 08:56:25 +0000

    add category page

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -2,10 +2,22 @@
   <div class="category">
     <Alert v-if="error" />
     <div v-if="loaded">
+      <my-image v-if="category" as="background" :alt="category.name" :src="category.image.url" />
       <div class="container">
-        <Breadcrumb :label="category.name" />
+        <Breadcrumb :label="`Categories / ${category.name}`" />
         <h1>{{category.name}}</h1>
+        <Card :article="article" v-for="article in articles" :key="article.name" />
+        <Center v-if="articles.length > 0">
+          <Button
+            :click="fetchMoreArticles"
+            :disabled="articles.length === total"
+            label="Load more"
+          />
+        </Center>
       </div>
+    </div>
+    <div v-else-if="!loaded && !error">
+      <Spinner />
     </div>
   </div>
 </template>
@@ -17,36 +29,95 @@ import VueAxios from 'vue-axios';
 
 import Alert from '@/components/Alert.vue';
 import Breadcrumb from '@/components/Breadcrumb.vue';
+import Button from '@/components/Button.vue';
+import Card from '@/components/Card.vue';
+import Center from '@/components/Center.vue';
+import MyImage from '@/components/Image.vue';
+import Spinner from '@/components/Spinner.vue';
 
 Vue.use(VueAxios, axios);
+
+const START = 0;
+const PER_PAGE = 2;
 
 export default {
   name: 'Category',
   components: {
     Alert,
     Breadcrumb,
+    Button,
+    Card,
+    Center,
+    MyImage,
+    Spinner,
   },
   data() {
     return {
+      articles: [],
       category: {},
+      end: PER_PAGE,
       error: false,
       loaded: false,
+      start: START,
+      total: null,
     };
   },
-  created() {
-    this.fetchCategory();
-  },
   methods: {
-    fetchCategory() {
-      const category = this.$route.params.pathMatch.replace(/-/g, ' ');
-      axios.get(`${process.env.VUE_APP_API_PATH}/categories?name=${category}`).then((response) => {
-        // eslint-disable-next-line prefer-destructuring
-        this.category = response.data[0];
+    fetchCategory(category) {
+      const data = `{
+        categories(where: { name: "${category}" }) {
+          name
+          image {
+            url
+          }
+          articles(limit: ${this.end}, start: ${this.start}) {
+            content
+            date
+            id
+            name
+            excerpt
+            image {
+              url
+            }
+          }
+        }
+      }`;
+      axios.get(`${process.env.VUE_APP_GRAPHQL_API_PATH}${data}`).then((response) => {
+        this.category = {
+          name: response.data.data.categories[0].name,
+          image: response.data.data.categories[0].image,
+        };
+        this.articles = this.articles.concat(response.data.data.categories[0].articles);
         this.loaded = true;
       }).catch(() => {
         this.error = true;
       });
     },
+    fetchTotal(category) {
+      const data = ` {
+        categories(where: { name: "${category}" }) {
+          articles {
+            id
+            name
+          }
+        }
+      }`;
+      axios.get(`${process.env.VUE_APP_GRAPHQL_API_PATH}${data}`).then((response) => {
+        this.total = response.data.data.categories[0].articles.length;
+      }).catch(() => {
+        this.error = true;
+      });
+    },
+    fetchMoreArticles() {
+      this.start += PER_PAGE;
+      this.end = this.start + PER_PAGE;
+      this.fetchCategory(this.category.name);
+    },
+  },
+  mounted() {
+    const category = this.$route.params.pathMatch.replace(/-/g, ' ').replace(/\/$/, '');
+    this.fetchCategory(category);
+    this.fetchTotal(category);
   },
 };
 </script>
